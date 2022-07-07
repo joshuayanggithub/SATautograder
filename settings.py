2dc40bf1c26427fc37fa3b28ac13b202b95794ad
Joshua Yang <85262856+joshuayanggithub@users.noreply.github.com>
2022-07-06 22:41:26 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-ShowAnswer = True #shows the correct answer when you get a question wrong; otherwise, simply tell you that your current answer is wrong and let the user figure out which is wrong
-test = "PracticeTest7" #which test you are doing?
-section = 1 #section 1,2,3,4 or alternatively reading, writing, math-no-calc, math-calc
+ShowAnswer = True #shows the correct answer when you get a question wrong; if you want to turn it off use False with UPPERCASE 'F' letter!!!!
+test = "PracticeTest7" #which test you are doing? make sure this string is IN quotations and matches EXACTLY the text in answers.txt
+section = 1 #section 1 or 2 alternatively reading, writing/ does not work with math sections yet
