e214c0dbca82165405b3e46b531468b1722c0977
Joshua Yang <85262856+joshuayanggithub@users.noreply.github.com>
2022-08-04 17:17:26 -0700

    Delete answerformatter.py

--- answerformatter.py
+++
@@ -1,8 +0,0 @@
-with open("unformatted.txt") as f:
-    lines = f.read()
-    lines = lines.replace('\n', ' ')
-    for word in lines.split(" "):
-        if (len(word) > 2):
-            lines = lines.replace(word,"")
-    print(lines)
-    
